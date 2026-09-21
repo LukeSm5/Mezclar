3601e6fc526a67fce5c65a30074a135e54b14282
gx1221 <georgex770@gmail.com>
2026-09-21 02:37:23 -0400

    Separated the creation of joincode from frontend since the backend should be generating codes

--- frontend-web/src/main.tsx
+++ frontend-web/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import LobbyView from './host/LobbyView';
+import { generateJoinCode } from './utils/lobby';
 import './styles.css';
 
-createRoot(document.getElementById('root')!).render(<LobbyView />);
+const demoJoinCode = generateJoinCode();
+
+createRoot(document.getElementById('root')!).render(
+  <LobbyView joinCode={demoJoinCode} />,
+);
